9850afac3b78843b759ef04524f3e50967c70e2a
clbowling12@gmail.com <clbowling12@gmail.com>
2018-06-18 16:53:23 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -6,4 +6,6 @@ def  display_rainbow(colors)  ["red", "orange", "yellow", "green", "blue", "indi
 
   puts "R:  #{red[0]}, O: #{orange[1]}, Y: #{yellow[2]}, G: #{green[3]}, B: #{blue[4]}, I: #{indigo[5]}, V: #{violet[6]}"
 
+
 end
+colors = ["red"]
